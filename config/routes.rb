c98dcb398eb47e3f5983b4f20de5588d1cfb8d3b
malikkcoleman <malikcoleman@live.ca>
2022-10-13 14:05:30 -0500

    Loaded all products into the products controller and displayed them in the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  # get 'products/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/products", to: "products#index"
 end
