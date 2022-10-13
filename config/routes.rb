effdf7c97a98e40359cee3f649c7295c6a6e907f
malikkcoleman <malikcoleman@live.ca>
2022-10-13 14:20:33 -0500

    Added show and link to index title to show view

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/products", to: "products#index"
+  get '/products/:id', to: "products#show"
 end
