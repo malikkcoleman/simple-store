127d740f5b89fbc05bc302eb86b48ab8a5ab9635
malikkcoleman <malikcoleman@live.ca>
2022-10-13 13:52:10 -0500

    Used Faker to add 676 products to the products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+counter = 1
+while counter <= 676
+  new_product = Product.create(
+                                title: Faker::Commerce.product_name,
+                                price: Faker::Commerce.price,
+                                stock_quantity: Faker::Number.digit
+                              )
+
+  counter = counter + 1
+end
